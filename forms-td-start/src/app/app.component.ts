4e7ecc5a1ae2d17f564450fe5871085f1619b91c
saumil <saumilsapariya@gmail.com>
2020-03-31 19:28:45 -0400

    Reset form

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -51,7 +51,7 @@ export class AppComponent {
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
 
-    this.signupForm.reset()
+    this.signupForm.reset();
 
   }
 }
