0644874dce311b82d11e69375ef12660a6bb17f3
Saumil Sapariya <saumilsapariya@gmail.com>
2020-04-02 19:11:48 -0400

    form-assignment

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  subscriptions = ['Basic', 'Advance', 'Pro'];
+  selectedSubscription = 'Advance';
+  submitted = false;
+
+  @ViewChild('signUpForm', { static: false }) sgnForm: NgForm;
+
+  formDetails = {
+    email: '',
+    subscription: ''
+  };
+
+  onSubmit() {
+    console.log(this.sgnForm.value);
+    this.submitted = true;
+
+    this.formDetails.email = this.sgnForm.value.email;
+    this.formDetails.subscription = this.sgnForm.value.subscription;
+    this.sgnForm.reset();
+  }
 }
