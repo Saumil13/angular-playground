d8e512ab5f48e149255c7ede3127b066e2a5916b
Saumil Sapariya <saumilsapariya@gmail.com>
2020-04-10 18:52:26 -0400

    observable to trasform data

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,17 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.onFetchData();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
       .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+        'https://ng-complete-guide-34e37.firebaseio.com/posts.json',
         postData
       )
       .subscribe(responseData => {
@@ -27,9 +30,26 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.onFetchData();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private onFetchData() {
+    this.http.get('https://ng-complete-guide-34e37.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postArray;
+      }))
+      .subscribe(data => {
+        console.log(data);
+      });
   }
 }
