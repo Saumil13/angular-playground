462815fa5946c595975fadbfc5184a2ff5d1bf6a
Saumil Sapariya <saumilsapariya@gmail.com>
2020-04-12 10:52:38 -0400

    sign up module with error exists validation

---
+++ prj-start/src/app/auth/auth.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(authForm: NgForm) {
+    if (!authForm.valid) {
+      return;
+    }
+
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+    this.isLoading = true;
+
+    if (this.isLoginMode) {
+
+    } else {
+      this.authService.signup(email, password).subscribe(resData => {
+        console.log(resData);
+        this.isLoading = false;
+      }, errorMessage => {
+        console.log(errorMessage);
+        this.isLoading = false;
+        this.error = errorMessage;
+      });
+    }
+    authForm.reset();
+  }
+
+}
---
+++ prj-start/src/app/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({ 'providedIn': 'root' })
+export class AuthService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  apiKey = 'AIzaSyBjqM3SfR7oCMvu-zVbc7wGmtNsGCjtw_I';
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.apiKey,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }).pipe(catchError(errorRes => {
+        let errorMessage = 'An unknow error occurred!';
+        if (!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+
+        switch (errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = 'This email exists already.';
+        }
+        return throwError(errorMessage);
+      }
+      ));
+  }
+
+}
