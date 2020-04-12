ad4854732ca37a3e562e4a0ceed0d6d3b6eec1a5
Saumil Sapariya <saumilsapariya@gmail.com>
2020-04-12 18:24:46 -0400

    Update auth.guard.ts

--- prj-start/src/app/auth/auth.guard.ts
+++ prj-start/src/app/auth/auth.guard.ts
@@ -2,7 +2,7 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTr
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
@@ -14,7 +14,7 @@ export class AuthGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, rotuer: RouterStateSnapshot)
     : boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
 
-    return this.authService.user.pipe(map(user => {
+    return this.authService.user.pipe(take(1), map(user => {
       const isAuth = !!user;
       if (isAuth) {
         return true;
