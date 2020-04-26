fce2c847ed6af67a2da52143803906314fcc440b
Saumil Sapariya <saumilsapariya@gmail.com>
2020-04-26 13:28:03 -0400

    add shopping list action using store

--- prj-start/src/app/shopping-list/store/shopping-list.action.ts
+++ prj-start/src/app/shopping-list/store/shopping-list.action.ts
@@ -2,6 +2,7 @@ import { Action } from '@ngrx/store';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
 export const ADD_INGERDIENT = 'ADD_INGERDIENT';
+export const ADD_INGERDIENTS = 'ADD_INGERDIENTS';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGERDIENT;
@@ -9,5 +10,14 @@ export class AddIngredient implements Action {
   constructor(public payload: Ingredient) {
 
   }
-
 }
+
+export class AddIngredients implements Action {
+  readonly type = ADD_INGERDIENTS;
+
+  constructor(public payload: Ingredient[]) {
+
+  }
+}
+
+export type ShoppingListActions = AddIngredient | AddIngredients;
