a18c2857e5cb3ff1cd07bfca4fde8bb5f3354036
Saumil Sapariya <saumilsapariya@gmail.com>
2020-04-27 22:31:13 -0400

    ngrx recipe edit

--- ngrx-project/src/app/shared/data-storage.service.ts
+++ ngrx-project/src/app/shared/data-storage.service.ts
@@ -4,14 +4,16 @@ import { map, tap, take, exhaustMap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
-import { AuthService } from '../auth/auth.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as RecipeActions from '../recipes/store/recipe.action';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
   constructor(
     private http: HttpClient,
     private recipeService: RecipeService,
-    private authService: AuthService
+    private store: Store<fromApp.AppState>
   ) { }
 
   storeRecipes() {
@@ -43,7 +45,8 @@ export class DataStorageService {
           });
         }),
         tap(recipes => {
-          this.recipeService.setRecipes(recipes);
+          // this.recipeService.setRecipes(recipes);
+          this.store.dispatch(new RecipeActions.SetRecipes(recipes));
         })
       );
   }
