5a2c2bbeef896bac6d55a3d27b30df1ef04bd3c0
Saumil Sapariya <saumilsapariya@gmail.com>
2020-05-03 16:50:41 -0400

    key update

--- ng-universal-02-finished/src/environments/environment.ts
+++ ng-universal-02-finished/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseAPIKey: 'AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y'
+  firebaseAPIKey: 'AIzaSyBjqM3SfR7oCMvu-zVbc7wGmtNsGCjtw_I'
 };
 
 /*
